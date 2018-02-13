af891b59f3c0ea012c0acc2d4f8409a4c78d149c
Eric Rowe <gkskillz@gmail.com>
2018-02-12 23:25:14 -0800

    Add admin datetime formatter

--- web/app/common.py
+++ web/app/common.py
@@ -1,6 +1,7 @@
 import os
-import jinja2
+import pytz
 
+import jinja2
 
 def _filename(f):
     """Return the filename relative to `common.py`."""
@@ -10,12 +11,24 @@ def _filename(f):
 SESSION_KEY_FILE = '../session_key.txt'
 TEMPLATE_DIR = 'template'
 
+ADMIN_LOCAL_TIMEZONE = pytz.timezone('US/Pacific')
+
+
+def admin_datetime_format(value):
+    if not value:
+        return u'\u2014'  # emdash
+    value = pytz.utc.localize(value)
+    value = value.astimezone(ADMIN_LOCAL_TIMEZONE)
+    return value.strftime('%Y/%m/%d %H:%M')
+
+
 JINJA_ENV = jinja2.Environment(
     loader=jinja2.FileSystemLoader(_filename(TEMPLATE_DIR)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True,
     trim_blocks=True,
 )
+JINJA_ENV.filters['admin_datetime'] = admin_datetime_format
 
 
 def session_key():
